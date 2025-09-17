1bcd43e4cfe4e9c1f707c68bd0ef1a055a005e60
Gindra-o7 <gilangindra734@gmail.com>
2025-09-17 15:35:45 +0700

    🗑️delete: remove Kartu Murojaah and Kartu Rekapan Muroja'ah PA routes from general router

--- src/routers/general.router.tsx
+++ src/routers/general.router.tsx
@@ -1,8 +1,6 @@
-import KartuMurojaahPage from "@/pages/publics/kartu-murojaah";
 import ForbiddenPage from "@/pages/publics/forbidden.page";
 import LandingPage from "@/pages/publics/landing.page";
 import NotFoundPage from "@/pages/publics/not-found.page";
-import KartuRekapanMurojaahPAPage from "@/pages/publics/kartu-rekapan-muroja'ah-pa";
 
 export const generalRouter = [
 	{
@@ -20,13 +18,5 @@ export const generalRouter = [
 	{
 		path: "*",
 		element: <NotFoundPage />,
-	},
-	{
-		path: "/kartu-murojaah/:id",
-		element: <KartuMurojaahPage />,
-	},
-	{
-		path: "/kartu-rekapan-murojaah-pa/:id",
-		element: <KartuRekapanMurojaahPAPage/>,
 	},
 ]

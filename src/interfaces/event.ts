9aa4618f3e4a372d544e999358473de494bb6a61
Gindra-o7 <gilangindra734@gmail.com>
2025-09-22 10:43:39 +0700

    ✨feat: Define EventType interface for event data structure

---
+++ src/interfaces/event.ts
@@ -0,0 +1,11 @@
+export type EventType = {
+  id: number;
+  title: string;
+  locate: string;
+  date: string;
+  duration: string;
+  price: string;
+  image: string;
+  gallery: string[];
+  description: string;
+};
